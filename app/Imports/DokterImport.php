a41452c1b5ef2658ebadc76827237bd7bb229b25
keyriz <41979457+keyriz@users.noreply.github.com>
2021-06-12 20:21:38 +0700

    Create import model

---
+++ app/Imports/DokterImport.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Dokter;
+use Illuminate\Support\Facades\Hash;
+use Maatwebsite\Excel\Concerns\ToModel;
+
+class DokterImport implements ToModel
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new Dokter([
+            'nama'    => $row[0],
+            'jabatan' => $row[1],
+        ]);
+    }
+}
