8792d4d2a01e6a5313357c4e48063eca4154772c
keyriz <41979457+keyriz@users.noreply.github.com>
2021-06-12 21:41:40 +0700

    Revisi models User

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,32 +12,20 @@ class User extends Authenticatable
     use HasFactory, Notifiable;
 
     /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
+    * The table associated with the model.
+    *
+    * @var string
+    */
+    protected $table = 'user';
+
+    /**
+    * The attributes that are mass assignable.
+    *
+    * @var array
+    */
     protected $fillable = [
         'name',
         'email',
         'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
     ];
 }
