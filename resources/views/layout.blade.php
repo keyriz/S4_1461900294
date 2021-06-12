6e2d707accdb079b88b62ea4c8a56d8b58802333
keyriz <41979457+keyriz@users.noreply.github.com>
2021-06-12 19:51:51 +0700

    Install tailwindcss

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8" />
+    <title>@yield('title')</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+<body class="bg-gray-100 font-sans">
+
+    @yield('content')
+
+</body>
+</html>
