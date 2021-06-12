f53e17336bf1447e6deec7954dcd547af10d6a13
keyriz <41979457+keyriz@users.noreply.github.com>
2021-06-12 21:40:57 +0700

    Update relation models Kamar

--- app/Models/Kamar.php
+++ app/Models/Kamar.php
@@ -24,4 +24,20 @@ class Kamar extends Model
     public $timestamps = false;
 
     protected $fillable = ['id_pasien', 'id_dokter'];
+
+    /**
+    * Get the pasien that owns the kamar.
+    */
+    public function pasien()
+    {
+        return $this->belongsTo(Pasien::class, 'id_pasien');
+    }
+
+    /**
+    * Get the dokter that owns the kamar.
+    */
+    public function dokter()
+    {
+        return $this->belongsTo(Dokter::class, 'id_dokter');
+    }
 }
